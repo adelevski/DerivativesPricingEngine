c5fdf5fa79769d655d0db244a120853f22baa2fb
Atanas Delevski <adelevski@gmail.com>
2021-03-08 16:49:59 -0600

    fixed problem with deconstructor, starting new vid

--- tutorials/BSM/BSM.cpp
+++ tutorials/BSM/BSM.cpp
@@ -1,1 +1,31 @@
 #include "BSM.h"
+
+
+BSM::BSM(float ass, float strk, float grwth, float volty, float yrs, long steps, long sims)
+{
+    bsmAsset = ass;
+    bsmStrike = strk;
+    bsmGrowth = grwth;
+    bsmVol = volty;
+    bsmYears = yrs;
+    bsmSteps = steps;
+    bsmMonteCarloSims = sims;
+}
+
+BSM::~BSM(){std::cout << "Destroyed";}
+
+void BSM::logNormalRandomWalk(){}
+double BSM::rn(){return 0.0;}
+
+float BSM::getBsmAsset(){return bsmAsset;}
+float BSM::getBsmStrike(){return bsmStrike;}
+float BSM::getBsmGrowth(){return bsmGrowth;}
+float BSM::getBsmVol(){return bsmVol;}
+float BSM::getBsmYears(){return bsmYears;}
+long BSM::getBsmSteps(){return bsmSteps;}
+long BSM::getBsmMonteCarloSims(){return bsmMonteCarloSims;}
+
+double BSM::getCallPrice(){return bsmCallPrice;}
+double BSM::getPutPrice(){return bsmPutPrice;}
+
+
--- tutorials/BSM/BSM.h
+++ tutorials/BSM/BSM.h
@@ -8,10 +8,34 @@ class BSM
     public:
         BSM(float, float, float, float, float, long, long);
 
-        ~BSM()
-        {
-            std::cout << "Destroyed";
-        }
+        ~BSM();
+
+        float getBsmAsset();
+        float getBsmStrike();
+        float getBsmGrowth();
+        float getBsmVol();
+        float getBsmYears();
+        long getBsmSteps();
+        long getBsmMonteCarloSims();
+
+        void logNormalRandomWalk();
+        double getCallPrice();
+        double getPutPrice();
+
+        double rn(); 
+
+    private:
+        
+        float bsmAsset;
+        float bsmStrike;
+        float bsmGrowth;
+        float bsmVol;
+        float bsmYears;
+        long bsmSteps;
+        long bsmMonteCarloSims;
+
+        double bsmCallPrice;
+        double bsmPutPrice;
 };
 
 #endif
