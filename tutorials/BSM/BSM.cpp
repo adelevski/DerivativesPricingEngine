bdb4f42d32264c6a0f8a106c5192a43212fc9c78
Atanas Delevski <adelevski@gmail.com>
2021-03-08 21:36:19 -0600

    fixed bug with double loop, now bsm random walk is working

--- tutorials/BSM/BSM.cpp
+++ tutorials/BSM/BSM.cpp
@@ -1,4 +1,6 @@
 #include "BSM.h"
+#include <math.h>
+#include <time.h>
 
 
 BSM::BSM(float ass, float strk, float grwth, float volty, float yrs, long steps, long sims)
@@ -14,8 +16,50 @@ BSM::BSM(float ass, float strk, float grwth, float volty, float yrs, long steps,
 
 BSM::~BSM(){std::cout << "Destroyed";}
 
-void BSM::logNormalRandomWalk(){}
-double BSM::rn(){return 0.0;}
+/*
+
+S{PHI}(d1) - Ke-rT{PHI}(d2) 
+
+Asset price 100, Call option, strike price 110
+
+100 -> 101 -> 99 -> 110 -> 125   (15)
+
+100 -> 98 -> 97 -> 54 -> 109      (0)
+
+100 -> 97 -> 112 -> 116 -> 116    (6) +
+                                -----
+                                  21/3 => 7
+
+
+ */                               
+
+void BSM::logNormalRandomWalk()
+{
+    srand((unsigned)time(0));
+
+    double callPayoffPot = 0.0;
+    double putPayoffPot = 0.0;
+
+    double timeStep = (getBsmYears()/getBsmSteps());
+    double sqrtTs = sqrt(timeStep);
+
+    for (long i = 1; i <= getBsmMonteCarloSims(); i++)
+    {
+        double ass = getBsmSteps();
+
+        for(int j = 1; j <= getBsmSteps(); j++)
+        {
+            ass = ass * (1 + getBsmGrowth()*timeStep + getBsmVol()*sqrtTs*(12*rn() - 6));
+
+            std::cout << "Step: " << j << " ass: " << ass << std::endl;
+        }
+    }
+}
+
+double BSM::rn()
+{
+    return (double)rand()/(double)(RAND_MAX + 1.0);
+}
 
 float BSM::getBsmAsset(){return bsmAsset;}
 float BSM::getBsmStrike(){return bsmStrike;}
