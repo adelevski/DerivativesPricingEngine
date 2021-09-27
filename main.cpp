729e81ab3325a194ac1ecf16f3c5744fd9e0776c
Atanas Delevski <adelevski@gmail.com>
2021-09-27 14:37:56 -0500

    added while loop

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include "output.hpp"
 
+#include <iostream>
+
+
+char repeat()
+{
+    char choice;
+    std::cout << "Would you like to price another option? [y/n]: ";
+    std::cin >> choice;
+    return choice; 
+}
 
 int main()
 {
-    input in = get_input();
-    sim_prices sp = monte_carlo(in);
-    cf_prices cp = black_scholes(in);
-    print_results(in, sp, cp);
+    while (true)
+    {
+        input in = get_input();
+        sim_prices sp = monte_carlo(in);
+        cf_prices cp = black_scholes(in);
+        print_results(in, sp, cp);
+        char choice = repeat();
+        if (choice != 'y')
+        {
+            break;
+        }
+    }
 }
 
